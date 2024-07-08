1a9541296f9b566fd7f0d07673fc6c445bcf35aa
Oskar Klintrot <oskar.klintrot@gmail.com>
2024-07-08 09:35:15 +0200

    feat: Add more tests to play around with

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,10 @@
 namespace xUnit1012;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Usage",
+    "xUnit1012:Null should only be used for nullable parameters",
+    Justification = "<Pending>"
+)]
 public class UnitTest1
 {
     [Theory]
@@ -7,4 +12,18 @@ public class UnitTest1
     [InlineData("Works")]
     [InlineData("Works", "as", "well")]
     public void Test1(params string[]? _) { }
+
+    [Theory]
+    [InlineData(null)]
+    public void Test2(params string[]? arr)
+    {
+        Assert.Null(arr);
+    }
+
+    [Theory]
+    [InlineData]
+    public void Test3(params string[]? arr)
+    {
+        Assert.Empty(arr!);
+    }
 }
